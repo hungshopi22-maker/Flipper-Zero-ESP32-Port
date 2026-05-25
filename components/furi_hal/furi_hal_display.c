8be9b2e00411259957ff5e4efe76890117faffd1
hungshopi22-maker <hungshopi22@gmail.com>
2026-05-25 18:38:06 +0700

    Update furi_hal_display.c

--- components/furi_hal/furi_hal_display.c
+++ components/furi_hal/furi_hal_display.c
@@ -223,7 +223,7 @@ void furi_hal_display_init(void) {
     ESP_ERROR_CHECK(esp_lcd_panel_io_tx_param(io_handle, 0x01 /* SWRESET */, NULL, 0));
     vTaskDelay(pdMS_TO_TICKS(150));
 
-    ESP_ERROR_CHECK(esp_lcd_new_panel_vendor(io_handle, &panel_config, &panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(io_handle, &panel_config, &panel_handle));
 
     
     /* Reset and initialize (the esp_lcd driver reset is redundant now but harmless) */
