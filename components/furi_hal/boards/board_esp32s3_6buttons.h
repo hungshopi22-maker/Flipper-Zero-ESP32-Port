561c6ba63d6bdd9b4167cb2fa87c5093f3805163
hungshopi22-maker <hungshopi22@gmail.com>
2026-05-25 17:11:15 +0700

    Update board_esp32s3_6buttons.h

--- components/furi_hal/boards/board_esp32s3_6buttons.h
+++ components/furi_hal/boards/board_esp32s3_6buttons.h
@@ -37,8 +37,8 @@
 #define BOARD_PIN_LCD_BL        6
 
 /* ---- LCD Display Configuration ---- */
-#define BOARD_LCD_H_RES         320     /* Native width after swap_xy */
-#define BOARD_LCD_V_RES         240     /* Native height after swap_xy */
+#define BOARD_LCD_H_RES         240     /* Native width after swap_xy */
+#define BOARD_LCD_V_RES         135     /* Native height after swap_xy */
 #define BOARD_LCD_SPI_HOST      SPI2_HOST
 #define BOARD_LCD_SPI_FREQ_HZ   (20 * 1000 * 1000)
 #define BOARD_LCD_CMD_BITS      8
